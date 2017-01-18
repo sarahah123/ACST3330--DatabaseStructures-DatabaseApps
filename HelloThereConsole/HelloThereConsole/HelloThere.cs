0f2e128abc857d46e049ca7df916cd41384db5cc
shansber <shansber55@gmail.com>
2017-01-17 21:06:48 -0500

    Add Lab 1

---
+++ HelloThereConsole/HelloThereConsole/HelloThere.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Course:          ACST 3330
+ * Section:         1
+ * Name:            Sarah Hansberry
+ * Professor:       Nagappan
+ * Assignment #:    Lab 1
+ */
+
+namespace HelloThereConsole
+{
+    class HelloThere
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello There");
+            Console.WriteLine("What's your name?");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hello {0}", name);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HelloThereMVC/HelloThereMVC/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloThereMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello There";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string username)
+        {
+            // Add code to make the program say hello to username
+
+            ViewBag.Message = "Hello " + username;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About HelloThereMVC.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
